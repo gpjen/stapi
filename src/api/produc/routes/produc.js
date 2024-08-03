2f2b5d6a79282b83b828f4ecdc3dd5ca5f4953e9
gpjen <gpjen95@gmail.com>
2024-08-04 00:15:39 +0700

    init

---
+++ src/api/produc/routes/produc.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * produc router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::produc.produc');
---
+++ src/api/produc/services/produc.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * produc service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::produc.produc');
